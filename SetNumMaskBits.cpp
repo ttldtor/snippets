2d66c8877da2e671f2d023dcfe575dd506867ac4
ttldtor <ttldtor@gmail.com>
2021-03-04 22:05:42 +0300

    Update SetNumMaskBits.cpp

--- SetNumMaskBits.cpp
+++ SetNumMaskBits.cpp
@@ -2,7 +2,7 @@
 #include <type_traits>
 
 template <typename T>
-constexpr T set_num_mask_bits3(std::size_t len) {
+constexpr T setNumMaskBits(std::size_t len) {
     using U = typename std::make_unsigned<T>::type;
     
     return len >= sizeof(T) * 8 ? std::numeric_limits<T>::max(): static_cast<T>((U(1) << len) - U(1));
